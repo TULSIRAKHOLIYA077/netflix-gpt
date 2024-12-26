fea05d84fdd993fdb2ada714181fc5b00c4af3b7
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2024-12-26 19:29:09 +0530

    code of adding data in redux store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,11 +3,13 @@ import Header from "./Header";
 import {checkValidData} from "../utils/validate";
 import {  createUserWithEmailAndPassword, signInWithEmailAndPassword  } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage,setErrorMessage] = useState(null);
+  const navigate = useNavigate();
 
   const name = useRef(null);
   const email = useRef(null);//initial value is null
@@ -30,6 +32,7 @@ const Login = () => {
           // Signed up 
           const user = userCredential.user;
           console.log(user);
+          navigate("/browse")
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -43,6 +46,7 @@ const Login = () => {
       // Signed in 
       const user = userCredential.user;
       console.log(user);
+      navigate("/browse")
       
     })
     .catch((error) => {
