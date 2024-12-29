e871f0a348c77549c29cd525f1f34a3e60505b85
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2024-12-29 17:14:07 +0530

    fixing bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,53 @@
+
 import logo from "../Asset/logo.png";
-import userImage from "../Asset/userImage.png";
-import { signOut } from "firebase/auth";
+ import userImage from "../Asset/userImage.png";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
+
 
 const Header = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const user = useSelector(store => store.user);
   const handleSignOut = () =>{
     signOut(auth).then(() => {
       // Sign-out successful.
-      navigate("/");
     }).catch((error) => {
       // An error happened.
       navigate("/error");
     });
   }
 
+    //put it here cause we want to keep it always render and Header component will always stay . Did all the changes cause when i sign in or sign out onAuthChanged keeps track
+    useEffect(()=>{
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          const {uid, email, displayName, photoURL} = user;
+          dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL:photoURL}));
+          navigate("/browse")
+        } else {
+          // User is signed out
+          dispatch(removeUser());
+          navigate("/")
+          }
+      });
+    }, [])
+  
+
   return (
     <div className="absolute w-[100%]  z-10 px-8 py-4 bg-black bg-opacity-50 flex justify-between items-center">
       <img className="w-28 h-12" src={logo} alt="" />
       <div className="flex">
-        <img className="w-9 h-9" src={userImage} alt="" />
+        {user && user.photoURL ? (
+          <img className="w-9 h-9" src={user.photoURL} alt="User" />
+        ) : (
+          <img className="w-9 h-9" src={userImage} alt="Default User" /> // Fallback image if user is null
+        )}
         <button 
         className="font-bold text-white"
         onClick={handleSignOut}
@@ -31,4 +58,6 @@ const Header = () => {
 }
 
 export default Header;
+
+
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,6 @@ import Header from "./Header";
 import {checkValidData} from "../utils/validate";
 import {  createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile  } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
@@ -11,7 +10,6 @@ import { addUser } from "../utils/userSlice";
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage,setErrorMessage] = useState(null);
-  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const name = useRef(null);
@@ -34,13 +32,13 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up 
           const user = userCredential.user;
+          
           updateProfile(user, {
-            displayName: name.current.value, photoURL: "https://example.com/jane-q-user/profile.jpg"
+            displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/124981059?v=4"
           }).then(() => {
             // Profile updated!
             const {uid, email, displayName, photoURL} = auth.currentUser;
             dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL:photoURL}));
-            navigate("/browse")
           }).catch((error) => {
             // An error occurred
             setErrorMessage(error.message)
@@ -58,9 +56,7 @@ const Login = () => {
     .then((userCredential) => {
       // Signed in 
       const user = userCredential.user;
-      console.log(user);
-      navigate("/browse")
-      
+      console.log(user);      
     })
     .catch((error) => {
       const errorCode = error.code;
