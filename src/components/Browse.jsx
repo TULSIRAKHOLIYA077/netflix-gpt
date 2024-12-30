d602e0e88b1dedaacb3d792d080665aafb322c5d
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2024-12-30 18:51:59 +0530

    adding movies in store but not as null but 20 object

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,14 +1,18 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1",API_OPTIONS);//removed language
 
     const json = await data.json();
     console.log(json.results);// it will logs 2 times cause of react's strict mode if i remove it from index.js it will log once
 
+    dispatch(addNowPlayingMovies(json.results));
     //have to call this function in useEffect
   }
 
