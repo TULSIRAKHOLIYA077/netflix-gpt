b7cfefa43dafa5ea743bcf6aac445ac672ea205c
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2024-12-30 18:16:48 +0530

    about StrictMode

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -7,7 +7,7 @@ const Browse = () => {
     const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1",API_OPTIONS);//removed language
 
     const json = await data.json();
-    console.log(json);
+    console.log(json.results);// it will logs 2 times cause of react's strict mode if i remove it from index.js it will log once
 
     //have to call this function in useEffect
   }
