1221c8a4fc55a9d3a80e89c7cc1f5d10e23df448
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2024-12-31 17:28:50 +0530

    working on browse page

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,27 +1,15 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../utils/moviesSlice";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-  const getNowPlayingMovies = async () => {
-    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1",API_OPTIONS);//removed language
-
-    const json = await data.json();
-    console.log(json.results);// it will logs 2 times cause of react's strict mode if i remove it from index.js it will log once
-
-    dispatch(addNowPlayingMovies(json.results));
-    //have to call this function in useEffect
-  }
-
-  useEffect(()=>{
-    getNowPlayingMovies();
-  },[]);
+  useNowPlayingMovies(); 
   return (
     <div>
       <Header/>
+      <MainContainer/>
+      <SecondaryContainer/>
     </div>
   )
 }
---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () => {
+
+    const data = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1",API_OPTIONS);//removed language
+
+    const json = await data.json();
+    console.log(json.results);// it will logs 2 times cause of react's strict mode if i remove it from index.js it will log once
+
+    dispatch(addNowPlayingMovies(json.results));
+    //have to call this function in useEffect
+  }
+
+  useEffect(()=>{
+    getNowPlayingMovies();
+  },[]);
+
+}
+
+export default useNowPlayingMovies;
